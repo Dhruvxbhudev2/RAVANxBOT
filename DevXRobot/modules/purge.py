59b6e774f3a482b8ff3bff105bc1d22d811672eb
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2021-12-02 18:47:10 +0530

    Update purge.py

--- DevXRobot/modules/purge.py
+++ DevXRobot/modules/purge.py
@@ -63,7 +63,7 @@ async def delete_messages(event):
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Whadya want to delete?")
+        await event.reply("🤪Successfully Deleted messages👻")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
