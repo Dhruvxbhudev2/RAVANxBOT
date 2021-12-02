29152207f4178ba4d8cbcc356ec05d85883253e1
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2021-12-02 18:53:55 +0530

    Update logomaker.py

--- DevXRobot/modules/logomaker.py
+++ DevXRobot/modules/logomaker.py
@@ -278,9 +278,9 @@ async def lego(event):
     x = (image_widthz-w)/2
     y = ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
-    fname = "DevXRobot.png"
+    fname = "RAVAN-BOT.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by @DevXRobot🥳")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by @ravan_the_grp_managerbot 🙃")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
