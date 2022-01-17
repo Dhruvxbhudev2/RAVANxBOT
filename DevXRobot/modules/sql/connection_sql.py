25a4ddac1c2ba6f1fda0952ab3b892da167c0e1f
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:24:58 +0530

    Update connection_sql.py

--- DevXRobot/modules/sql/connection_sql.py
+++ DevXRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from DevXRobot.modules.sql import SESSION, BASE
+from RAVANxBOT.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
