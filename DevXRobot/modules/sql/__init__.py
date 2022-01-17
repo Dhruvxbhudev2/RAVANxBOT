532ed5e9ca1790197f09e4e7924182a090322567
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:16:33 +0530

    Update __init__.py

--- DevXRobot/modules/sql/__init__.py
+++ DevXRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from DevXRobot import DB_URI
+from RAVANxBOT import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
