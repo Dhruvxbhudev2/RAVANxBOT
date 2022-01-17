7be3da0e420bc5cb241bb865931bb147d6649fab
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:36:32 +0530

    Update forceSubscribe_sql.py

--- DevXRobot/modules/sql_extended/forceSubscribe_sql.py
+++ DevXRobot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from DevXRobot.modules.sql import BASE, SESSION
+from RAVANxBOT.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
