717d108894c4ed37edc148280a98af39daf71907
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:08:48 +0530

    Update extraction.py

--- DevXRobot/modules/helper_funcs/extraction.py
+++ DevXRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from DevXRobot import LOGGER
-from DevXRobot.modules.users import get_user_id
+from RAVANxBOT import LOGGER
+from RAVANxBOT.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
