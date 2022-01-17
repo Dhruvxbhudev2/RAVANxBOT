907741780860a033d3eccd83fd0edfe05fc59eee
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:21:35 +0530

    Update blacklist_sql.py

--- DevXRobot/modules/sql/blacklist_sql.py
+++ DevXRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from DevXRobot.modules.sql import SESSION, BASE
+from RAVANxBOT.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
