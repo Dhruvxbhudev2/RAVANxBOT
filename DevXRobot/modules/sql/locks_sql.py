e2234531384a1374f15b1579671866c65d611b78
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:29:07 +0530

    Update locks_sql.py

--- DevXRobot/modules/sql/locks_sql.py
+++ DevXRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from DevXRobot.modules.sql import SESSION, BASE
+from RAVANxBOT.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
