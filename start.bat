669e57ca1fa748f5ba68fd744bfde5b37ea948b5
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:01:44 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE DevX Robot
+TITLE RAVANxBOT
 :: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m DevXRobot
+env\scripts\activate.bat && py -m RAVANxBOT
