72df94985d67ac4572f06c15c947c878f03881c1
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-18 02:21:09 +0530

    Update approve_sql.py

--- DevXRobot/modules/sql/approve_sql.py
+++ DevXRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from DevXRobot.modules.sql import BASE, SESSION
+from RAVANxBOT.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
