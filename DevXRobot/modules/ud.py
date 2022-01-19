1f1c35b9d41a445bf283c543d778c00bfcdbc2c8
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-19 16:21:06 +0530

    Update ud.py

--- DevXRobot/modules/ud.py
+++ DevXRobot/modules/ud.py
@@ -1,6 +1,6 @@
 import requests
-from DevXRobot import dispatcher
-from DevXRobot.modules.disable import DisableAbleCommandHandler
+from RAVANxBOT import dispatcher
+from RAVANxBOT.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
