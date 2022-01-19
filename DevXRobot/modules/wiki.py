d3d1dea825627b7e3122a9d3816f5b42ddd81282
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-19 14:52:54 +0530

    Update wiki.py

--- DevXRobot/modules/wiki.py
+++ DevXRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from DevXRobot import dispatcher
-from DevXRobot.modules.disable import DisableAbleCommandHandler
+from RAVANxBOT import dispatcher
+from RAVANxBOT.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
