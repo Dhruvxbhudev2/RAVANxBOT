e940e00073a5defb3f6efaaf418da36a25ac2c7a
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-19 14:52:12 +0530

    Update zombies.py

--- DevXRobot/modules/zombies.py
+++ DevXRobot/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from DevXRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from RAVANxBOT import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
@@ -114,7 +114,7 @@ async def zombies(event):
     
     
 from telethon.tl.types import UserStatusLastMonth, UserStatusLastWeek, ChatBannedRights
-from DevXRobot.events import register
+from RAVANxBOT.events import register
 from telethon import *
 from telethon.tl.functions.channels import (EditBannedRequest)
                                             
