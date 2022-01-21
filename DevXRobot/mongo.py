d390e6c167f03b39bf3f16dba84ac86683998f25
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-21 21:14:32 +0530

    Update mongo.py

--- DevXRobot/mongo.py
+++ DevXRobot/mongo.py
@@ -2,10 +2,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from DevXRobot import MONGO_DB_URI 
+from RAVANxBOT import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from DevXRobot.conf import get_int_key, get_str_key
+from RAVANxBOT.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
