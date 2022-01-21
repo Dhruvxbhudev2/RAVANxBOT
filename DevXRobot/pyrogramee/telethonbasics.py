b3950c17317e7405b1b5545bb2a6083f75d90a9e
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-21 21:07:39 +0530

    Update telethonbasics.py

--- DevXRobot/pyrogramee/telethonbasics.py
+++ DevXRobot/pyrogramee/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from DevXRobot.pyrogramee.pluginshelper import runcmd
+from RAVANxBOT.pyrogramee.pluginshelper import runcmd
 
 
 async def convert_to_image(event, borg):
