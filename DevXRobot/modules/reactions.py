e61deaa6df311562954ac56f985b6301c9811d82
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-21 20:27:33 +0530

    Update reactions.py

--- DevXRobot/modules/reactions.py
+++ DevXRobot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from DevXRobot import dispatcher
-from DevXRobot.modules.disable import DisableAbleCommandHandler
+from RAVANxBOT import dispatcher
+from RAVANxBOT.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
