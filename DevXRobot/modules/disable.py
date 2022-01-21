8634a9761c48eee15384f86b4b3fa185e7127a73
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-21 20:15:09 +0530

    Update disable.py

--- DevXRobot/modules/disable.py
+++ DevXRobot/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from DevXRobot import dispatcher
-from DevXRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from DevXRobot.modules.helper_funcs.misc import is_module_loaded
+from RAVANxBOT import dispatcher
+from RAVANxBOT.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from RAVANxBOT.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -20,12 +20,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from DevXRobot.modules.helper_funcs.chat_status import (
+    from RAVANxBOT.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from DevXRobot.modules.sql import disable_sql as sql
+    from RAVANxBOT.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
