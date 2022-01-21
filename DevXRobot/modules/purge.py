0dc9e703eca9aa045fa3165887c5c85ba6bc18f7
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-21 20:28:28 +0530

    Update purge.py

--- DevXRobot/modules/purge.py
+++ DevXRobot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from DevXRobot import telethn
-from DevXRobot.modules.helper_funcs.telethn.chatstatus import (
+from RAVANxBOT import telethn
+from RAVANxBOT.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
