54a51109a307f4b95e6132aa89d87b6b0dff42b0
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-23 15:48:28 +0530

    Update logomaker.py

--- RAVANxBOT/modules/logomaker.py
+++ RAVANxBOT/modules/logomaker.py
@@ -268,7 +268,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./DevXRobot/utils/Logo/*")
+    fnt = glob.glob("./RAVANxBOT/utils/Logo/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 120)
     w, h = draw.textsize(text, font=font)
